338246ad53f13ffef996bf956193af2de9e07fd4
fcarlone <frankcarlone@gmail.com>
2018-05-14 16:37:01 -0400

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessgeHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessgeHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  };
+};
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -18,6 +33,7 @@ socket.on('newMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 
   // var li = jQuery('<li></li>');
   // li.text(`${message.from} ${formattedTime}: ${message.text}`);
@@ -36,7 +52,8 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
-  
+  scrollToBottom();
+
   // var li = jQuery('<li></li>');
   // var a = jQuery('<a target="_blank">My current location</a>');
   //
